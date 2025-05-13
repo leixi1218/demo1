206e762ac8e64624e180904aa223f4c7f64c60ff
lei <2154991611@qq.com>
2025-05-13 11:01:23 +0800

    Create express.js
    创建express.js文件

---
+++ express.js
@@ -0,0 +1,15 @@
+// 导入express模块
+const express = require('express');
+
+// 创建Web服务器实例对象
+const app = express();
+
+// 创建请求地址为'/'的GET请求方式的路由
+app.get('/',(req,res) => {
+    res.send('<h1>你好，HelloWorld</h1>')
+});
+
+// 监听3000端口
+app.listen(3000,() => {
+    console.log('服务器已启动...');
+});
